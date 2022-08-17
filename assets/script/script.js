7c65ddc1bab96b292446210a14a54b2747862ee2
Geraldo Junior <105813128+geraldojunior03@users.noreply.github.com>
2022-08-17 09:58:43 -0300

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new TypeIt(".texto-animado", {
+        speed: 250,
+        loop: true
+    })
+    .type('Analista QA', {delay: 1000})
+    .delete(11)
+    .type('Desenvil', {delay: 500})
+    .delete(2)
+    .type('olvedore', {delay: 500})
+    .delete(1)
+    .type(' .NET', {delay: 1000})
+    .pause(1000)
+    .delete(4)
+    .type('C#', {delay: 500})
+    .delete(16)
+    .go();
+})
+
+console.clear();
+let items = document.querySelector(`.galeria-projetos`),
+    prev = document.querySelector(`.prev`),
+    next = document.querySelector(`.next`), current = 0, maxCurrent = items.children.length;
+
+prev.addEventListener('click', event => {
+  current--;
+  if (current < 0) current += maxCurrent;
+  
+  [...items.children].forEach(child => child.classList.remove('atual'));
+  
+  const currentChildren = items.children[current];
+  
+  currentChildren.classList.add('atual');
+  items.scrollTo(currentChildren.offsetLeft - (items.offsetWidth - currentChildren.offsetWidth) / 2, currentChildren.offsetTop);
+});
+
+next.addEventListener('click', event => {
+  current++;
+  if (current >= maxCurrent) current -= maxCurrent;
+  
+  [...items.children].forEach(child => child.classList.remove('atual'));
+  
+  const currentChildren = items.children[current];
+  
+  currentChildren.classList.add('atual');
+  items.scrollTo(currentChildren.offsetLeft - (items.offsetWidth - currentChildren.offsetWidth) / 2, currentChildren.offsetTop);
+});
+
+
